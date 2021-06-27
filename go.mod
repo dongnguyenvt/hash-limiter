d4e30281ccd4767d7b2da18728114203200bd847
Dong Nguyen <nguyenduydong@gmail.com>
2021-06-27 16:56:26 +0700

    fix github build

--- go.mod
+++ go.mod
@@ -3,9 +3,6 @@ module github.com/dongnguyenvt/hash-limiter
 go 1.11
 
 require (
-	github.com/onsi/ginkgo v1.16.4 // indirect
-	github.com/onsi/gomega v1.13.0 // indirect
 	github.com/segmentio/fasthash v1.0.3
 	github.com/stretchr/testify v1.7.0
-	github.com/tjarratt/babble v0.0.0-20210505082055-cbca2a4833c1
 )
--- limiter_test.go
+++ limiter_test.go
@@ -1,13 +1,16 @@
 package limiter
 
 import (
+	"math/rand"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/require"
-	"github.com/tjarratt/babble"
 )
 
-var babbler = babble.NewBabbler()
+func randomString() string {
+	return "some_string_" + strconv.Itoa(rand.Int())
+}
 
 func TestNewLimiter(t *testing.T) {
 	l, err := NewLimiter(0, 0)
@@ -28,7 +31,7 @@ func TestLimiter_Take(t *testing.T) {
 	l, _ := NewLimiter(1024, 10)
 	var cnt int
 	for i := 0; i < 100000; i++ {
-		ok, _ := l.Take(babbler.Babble(), babbler.Babble(), babbler.Babble())
+		ok, _ := l.Take(randomString(), randomString(), randomString())
 		if !ok {
 			cnt++
 		}
@@ -37,7 +40,7 @@ func TestLimiter_Take(t *testing.T) {
 	l, _ = NewLimiter(1024, 10)
 	cnt = 0
 	for i := 0; i < 100000; i++ {
-		ok, give := l.Take(babbler.Babble(), babbler.Babble(), babbler.Babble())
+		ok, give := l.Take(randomString(), "some string 2", "some string 3")
 		if !ok {
 			cnt++
 		} else {
@@ -47,10 +50,11 @@ func TestLimiter_Take(t *testing.T) {
 	t.Log(cnt)
 }
 
+//BenchmarkLimiter_Take-12    	 2229985	       496.9 ns/op
 func BenchmarkLimiter_Take(b *testing.B) {
 	l, _ := NewLimiter(1024, 10)
 	for i := 0; i < b.N; i++ {
-		ok, give := l.Take(babbler.Babble(), babbler.Babble(), babbler.Babble())
+		ok, give := l.Take(randomString(), randomString(), randomString())
 		if ok {
 			give()
 		}
